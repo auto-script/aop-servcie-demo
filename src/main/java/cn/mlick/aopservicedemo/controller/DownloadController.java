9f510266aff587e599e9bb947405b341cf84d9d7
xiangxin.li <xiangxin.li@envision-digital.com>
2020-05-20 13:48:43 +0800

    增加请求方法 2

--- src/main/java/cn/mlick/aopservicedemo/controller/DownloadController.java
+++ src/main/java/cn/mlick/aopservicedemo/controller/DownloadController.java
@@ -1,8 +1,10 @@
 package cn.mlick.aopservicedemo.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -65,7 +67,42 @@ public class DownloadController {
 
         Map<String, Object> stringObjectMap = TestCtrl.putAllHeaders(request, null, map1);
         Path path = Files
-                .write(Paths.get("/data/file/" + UUID.randomUUID() + ".txt"), JSON.toJSONString(stringObjectMap)
+                .write(Paths.get("/data/file/" + UUID.randomUUID() + ".txt"), JSON.toJSONString(stringObjectMap, SerializerFeature.PrettyFormat)
+                        .getBytes());
+        File file = path.toFile();
+
+        byte[] body;
+        InputStream is = new FileInputStream(file);
+        body = new byte[is.available()];
+        try {
+            int read = is.read(body);
+            System.out.println(read);
+        } catch (IOException e) {
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attchement;filename=" + file.getName());
+        HttpStatus statusCode = HttpStatus.OK;
+        ResponseEntity<byte[]> entity = new ResponseEntity<byte[]>(body, headers, statusCode);
+
+        try {
+            is.close();
+        } catch (IOException ignored) {
+        }
+
+        return entity;
+    }
+
+
+    @PostMapping(value = "/getFileForm", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<byte[]> getFileForm(String name, String nick,
+                                              HttpServletRequest request) throws IOException {
+        Map<String, Object> stringObjectMap = TestCtrl.putAllHeaders(request);
+        stringObjectMap.put("name", name);
+        stringObjectMap.put("nick", nick);
+
+        Path path = Files
+                .write(Paths.get("/data/file/" + UUID.randomUUID() + ".txt"), JSON.toJSONString(stringObjectMap, SerializerFeature.PrettyFormat)
                         .getBytes());
         File file = path.toFile();
 
